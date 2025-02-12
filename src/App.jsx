4e9fad893dc0fef473f895a6ca8f246316712563
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-02-11 22:08:49 -0300

    update project spotify

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import Header from "./components/Header"
-import Main from "./components/Main"
+import Artists from "./pages/Artists"
+import Artist from "./pages/Artist"
+import Songs from "./pages/Songs"
+import Song from "./pages/Song"
+import Home from "./pages/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   
 
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <Main/>
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/artists" element={<Artists/>}/>
+        <Route path="/artist/:id" element={<Artist/>}/>
+        <Route path="/songs" element={<Songs/>}/>
+        <Route path="/song/:id" element={<Song/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Artist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Artist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Artist
+
