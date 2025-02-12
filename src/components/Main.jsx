bb4511523b327a589282bc19aa0f2da94791d608
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-02-12 20:35:55 -0300

    update project spotify

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,20 @@ import ItemList from './ItemList';
 import { artistArray } from '../assets/database/artists';
 import { songsArray } from '../assets/database/songs';
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
         {/* Item list de Artistas */}
+        {type === 'artists' || type === undefined ? 
         <ItemList title="Artistas" items={5} itemsArray={artistArray} path='/artists' idPath='/artist'/>
+        : <></>}
+        
 
         {/* Item list de Musicas */}
-        <ItemList title="Músicas" items={10} itemsArray={songsArray} path='/songs' idPath='/song'/>
+        {type === 'songs' || type === undefined ? 
+        <ItemList title="Músicas" items={10} itemsArray={songsArray} path='/songs' idPath='/song'/> 
+        : <></> }
+        
     </div>
   )
 }
--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { Link } from 'react-router-dom';
 
 const Artist = () => {
   return (
-    <div>
+    <div className="artist">
+      <div className="artist__header" style={{backgroundImage: 
+        "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618600001016b37fbcbd078cb239588df5d9)", }}>
+
+      </div>
       
+      <div className="artist__body">
+
+      </div>
+
+      <Link to='/song/1'>
+        <FontAwesomeIcon className='single-item__icon  single-item__icon--artist' icon={faCirclePlay} />
+      </Link>
     </div>
   )
 }
