616c5aacf1dc8371a067de997c38d1ecc9a1c80e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-02-13 20:55:17 -0300

    UPDATE

---
+++ back-end/api/server.js
@@ -0,0 +1,23 @@
+// API
+import express from 'express'
+import {artistArray} from '../../front-end/src/assets/database/artists.js'
+import {songsArray} from '../../front-end/src/assets/database/songs.js'
+
+const app = express();
+const PORT = 3001
+
+app.get('/', (request, response) => {
+    response.send("Só vamos trabalhar com os endcleaspoints '/artists' e '/songs'")
+})
+
+app.get('/artists', (request, response) => {
+    response.send(artistArray)
+})
+
+app.get('/songs', (request, response) => {
+    response.send(songsArray)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor escutando na porta ${PORT}`)
+})
