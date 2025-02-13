5618d0fe4073e59b3564e0c57ecbb263d0eb9d31
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2025-02-12 23:50:08 -0300

    update projeto

---
+++ src/components/Player.jsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+import { faBackwardStep, faForwardStep } from '@fortawesome/free-solid-svg-icons'; // Para o ícone de retroceder
+import { Link } from 'react-router-dom';
+
+const Player = ({ duration, randomIdFromArtist, randomId2FromArtist }) => {
+    return (
+      <div className="player">
+        <div className="player__controllers">
+          <Link to={`/song/${randomIdFromArtist}`}>
+            <FontAwesomeIcon className="player__icon" icon={faBackwardStep} />
+          </Link>
+  
+          <FontAwesomeIcon
+            className="player__icon player__icon--play"
+            icon={faCirclePlay}
+          />
+  
+          <Link to={`/song/${randomId2FromArtist}`}>
+            <FontAwesomeIcon className="player__icon" icon={faForwardStep} />
+          </Link>
+        </div>
+  
+        <div className="player__progress">
+          <p>00:00</p>
+  
+          <div className="player__bar">
+            <div className="player__bar-progress"></div>
+          </div>
+  
+          <p>{duration}</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Player;
+  
